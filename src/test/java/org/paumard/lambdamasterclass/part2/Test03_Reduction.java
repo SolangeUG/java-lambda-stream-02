d13b559b3c307f4ce8843e003921ae99442c362a
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2019-02-06 10:13:23 +0000

    Write a factorial function using reduce

--- src/test/java/org/paumard/lambdamasterclass/part2/Test03_Reduction.java
+++ src/test/java/org/paumard/lambdamasterclass/part2/Test03_Reduction.java
@@ -3,6 +3,7 @@ package org.paumard.lambdamasterclass.part2;
 import org.junit.Test;
 
 import java.math.BigInteger;
+import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +13,11 @@ public class Test03_Reduction {
     public void reduction_1() {
         long number = 21;
 
-        BigInteger result = null; // TODO
+        BigInteger identity = BigInteger.ONE;
+
+        BigInteger result = LongStream.rangeClosed(1, number)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(identity, BigInteger::multiply);
 
         assertThat(result).isEqualTo(new BigInteger("51090942171709440000"));
     }
